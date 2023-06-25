62542d59b4d10d5dc562d17e7fd063a3981438c3
olawale-o <omoogunolawale@gmail.com>
2023-06-25 16:58:24 +0100

    Save records to mongodb

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"go-simple-rest/db"
 	"go-simple-rest/src"
 	"log"
 	"net/http"
@@ -8,7 +10,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func terminate() {
+	if r := recover(); r != nil {
+		fmt.Println("An error occured ", r)
+		fmt.Println("Application terminated gracefully")
+	} else {
+		fmt.Println("Application executed succcesfully")
+	}
+}
 func main() {
+	defer terminate()
+	client, ctx, err := db.Connect()
+	// defer cancel()
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
--- src/v1/articles/controller.go
+++ src/v1/articles/controller.go
@@ -1,30 +1,36 @@
 package articles
 
 import (
+	"go-simple-rest/db"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Article struct {
-	TITLE  string `json:"title"`
-	AUTHOR string `json:"author"`
-}
+var client, ctx, _ = db.Connect()
 
-var a []Article = []Article{
-	{TITLE: "Article 1", AUTHOR: "Author 1"},
-	{TITLE: "Article 2", AUTHOR: "Author 2"},
-	{TITLE: "Article 3", AUTHOR: "Author 3"},
-	{TITLE: "Article 4", AUTHOR: "Author 4"},
+var collection = client.Database("go").Collection("articles")
+
+type Article struct {
+	ID     string `bson:"_id,omitempty" json:"id,omitempty"`
+	TITLE  string `bson:"title" json:"title"`
+	AUTHOR string `bson:"author" json:"author"`
 }
 
 func GetArticles(c *gin.Context) {
-	// c.JSON(http.StatusOK, gin.H{
-	//  "articles": articles,
-	// })
-	c.IndentedJSON(http.StatusOK, a)
+	cursor, err := collection.Find(ctx, bson.D{{}})
+	if err != nil {
+		panic(err)
+	}
+	var articles []Article
+	if err = cursor.All(ctx, &articles); err != nil {
+		panic(err)
+	}
+	c.IndentedJSON(http.StatusOK, articles)
 }
 
 func NewArticle(c *gin.Context) {
@@ -33,46 +39,54 @@ func NewArticle(c *gin.Context) {
 		log.Println(err)
 		return
 	}
-	a = append(a, newArticle)
-	c.IndentedJSON(http.StatusCreated, newArticle)
+	doc := Article{TITLE: newArticle.TITLE, AUTHOR: newArticle.AUTHOR}
+	res, err := collection.InsertOne(ctx, doc)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	id := res.InsertedID
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "Article created", "id": id})
 }
 
 func ShowArticle(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
-	for i, article := range a {
-		if i == id {
-			c.IndentedJSON(http.StatusOK, article)
-			return
-		}
+	oid, _ := primitive.ObjectIDFromHex(c.Param("id"))
+	var article Article
+	filter := bson.M{"_id": oid}
+	if err := collection.FindOne(ctx, filter).Decode(&article); err != nil {
+		log.Println(err)
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Article not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"article": article})
 }
 
 func UpdateArticle(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
+	oid, _ := primitive.ObjectIDFromHex(c.Param("id"))
 	var updatedArticle Article
 	if err := c.BindJSON(&updatedArticle); err != nil {
 		log.Println(err)
 		return
 	}
-	for i := range a {
-		if i == id {
-			a[i] = updatedArticle
-			c.IndentedJSON(http.StatusOK, updatedArticle)
-			return
-		}
+	filter := bson.M{"_id": oid}
+	update := bson.M{"$set": bson.M{"title": updatedArticle.TITLE, "author": updatedArticle.AUTHOR}}
+	opts := options.Update().SetUpsert(true)
+
+	result, err := collection.UpdateOne(ctx, filter, update, opts)
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Article not found"})
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Article not found"})
+
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Article updated", "article": result})
 }
 
 func DeleteArticle(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
-	for i := range a {
-		if i == id {
-			a = append(a[:i], a[i+1:]...)
-			c.IndentedJSON(http.StatusNoContent, gin.H{"message": "Article deleted"})
-			return
-		}
+	oid, _ := primitive.ObjectIDFromHex(c.Param("id"))
+	filter := bson.M{"_id": oid}
+	result, err := collection.DeleteOne(ctx, filter)
+	if err != nil {
+		log.Println(err)
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Article not found"})
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Article not found"})
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Article deleted", "article": result})
 }
