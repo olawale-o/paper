e7882734f791b70507f48683682b7762f77e5ddf
olawale-o <omoogunolawale@gmail.com>
2025-01-02 16:59:20 +0100

    Use replica sets

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"go-simple-rest/db"
 	"go-simple-rest/src"
 	"log"
+	"sync"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func terminate() {
@@ -17,15 +21,31 @@ func terminate() {
 		fmt.Println("Application executed succcesfully")
 	}
 }
-func main() {
-	defer terminate()
-	client, ctx, err := db.Connect()
-	// defer cancel()
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
+
+func iterateChangeStream(routineCtx context.Context, waitGroup sync.WaitGroup, stream *mongo.ChangeStream) {
+	defer stream.Close(routineCtx)
+	defer waitGroup.Done()
+	for stream.Next(routineCtx) {
+		var data bson.M
+		if err := stream.Decode(&data); err != nil {
 			panic(err)
 		}
-	}()
+		fmt.Printf("%v\n", data)
+	}
+}
+
+func init() {
+
+	// defer terminate()
+	db.Connect()
+	// defer func() {
+	// 	if err = client.Disconnect(ctx); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+}
+
+func main() {
 	r := gin.Default()
 	src.Routes(r)
 	log.Println("Starting server... on port ", 8080)
