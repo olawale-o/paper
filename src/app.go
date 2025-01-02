cd5fd3ef3f947f828b062c0e73bdc7dd8b12c54a
olawale-o <omoogunolawale@gmail.com>
2025-01-02 09:16:56 +0100

    Update routes

--- src/app.go
+++ src/app.go
@@ -2,10 +2,12 @@ package src
 
 import (
 	"go-simple-rest/src/v1/articles"
+	"go-simple-rest/src/v1/auth"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(r *gin.Engine) {
 	articles.ArticleRoutes(r)
+	auth.AuthRoutes(r)
 }
