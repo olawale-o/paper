44efeed0880e4cf0b4b3003978301d62e8a58e40
olawale-o <omoogunolawale@gmail.com>
2025-01-02 16:45:56 +0100

    Update article struct

--- src/v1/articles/struct.go
+++ src/v1/articles/struct.go
@@ -1,12 +1,18 @@
 package articles
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Article struct {
-	ID        string `bson:"_id,omitempty" json:"id,omitempty"`
-	TITLE     string `bson:"title" json:"title"`
-	CONTENT   string `bson:"content" json:"content"`
-	AUTHORID  string `bson:"authorId,omitempty" json:"authorId,omitempty"`
-	LIKES     int    `bson:"likes,omitempty" json:"likes,omitempty"`
-	VIEWS     int    `bson:"views,omitempty" json:"views,omitempty"`
-	CREATEDAT string `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
-	UPDATEDAT string `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
+	ID        interface{}        `bson:"_id,omitempty" json:"id,omitempty"`
+	TITLE     string             `bson:"title" json:"title"`
+	CONTENT   string             `bson:"content" json:"content"`
+	AUTHORID  primitive.ObjectID `bson:"authorId,omitempty" json:"authorId,omitempty"`
+	LIKES     int                `bson:"likes,omitempty" json:"likes,omitempty"`
+	VIEWS     int                `bson:"views,omitempty" json:"views,omitempty"`
+	CREATEDAT time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	UPDATEDAT time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
 }
