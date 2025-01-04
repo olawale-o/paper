6a21a9f8ba336bd620c08ffa7a2995f3c43c4a81
olawale-o <omoogunolawale@gmail.com>
2025-01-04 03:41:06 +0100

    Create verifyToken helper function

--- src/v1/jwt/jwt.go
+++ src/v1/jwt/jwt.go
@@ -40,3 +40,17 @@ func createToken(payload string) (string, error) {
 	}
 	return tokenString, nil
 }
+
+func verifyToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+	return token, nil
+}
