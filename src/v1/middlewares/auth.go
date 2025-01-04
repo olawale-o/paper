8b08adb45827d09695ba9c8eca68b8d6a094ded9
olawale-o <omoogunolawale@gmail.com>
2025-01-04 04:27:10 +0100

    Implement auth middleware

--- src/v1/middlewares/auth.go
+++ src/v1/middlewares/auth.go
@@ -1,16 +1,31 @@
 package middlewares
 
 import (
+	"fmt"
+	"go-simple-rest/src/v1/jwt"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Set example variable
-		c.Set("userId", "12345")
+		tokenString, err := c.Cookie("token")
 
+		if err != nil {
+			fmt.Println("Token is missing")
+			c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "Missing token"})
+			return
+		}
+
+		token, err := jwt.VerifyToken(tokenString)
+		if err != nil {
+			fmt.Printf("Token verification failed: %v\\n", err)
+			c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "Unathorized"})
+			return
+		}
+		fmt.Printf("Token verified successfully. Claims: %+v\\n", token.Claims)
 		// before request
 
 		c.Next()
