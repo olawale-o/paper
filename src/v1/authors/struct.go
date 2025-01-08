24c6faa78757928f577607a61a8549e84a6bf558
olawale-o <omoogunolawale@gmail.com>
2025-01-08 12:31:47 +0100

    Modify authors struct with User struct

--- src/v1/authors/struct.go
+++ src/v1/authors/struct.go
@@ -1,8 +1,15 @@
 package authors
 
-type Author struct {
-	ID        string        `bson:"_id,omitempty" json:"id,omitempty"`
-	USERID    string        `bson:"userId,omitempty" json:"userId,omitempty"`
-	ARTICLES  []interface{} `bson:"articles,omitempty" json:"articles,omitempty"`
-	CREATEDAT string        `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+import "go-simple-rest/src/v1/articles"
+
+type User struct {
+	ID                string             `bson:"_id,omitempty" json:"id,omitempty"`
+	FIRSTNAME         string             `bson:"firstName" json:"firstName"`
+	LASTNAME          string             `bson:"lastName" json:"lastName"`
+	USERNAME          string             `bson:"username" json:"username" binding:"required"`
+	PASSWORD          string             `bson:"password" json:"password"`
+	ARTICLECOUNT      int                `bson:"articleCount,omitempty" json:"articleCount,omitempty"`
+	ARTICLELIKESCOUNT int                `bson:"articleLikesCount,omitempty" json:"articleLikesCount,omitempty"`
+	CREATEDAT         string             `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	ARTICLES          []articles.Article `bson:"articles,omitempty" json:"articles,omitempty"`
 }
