5b52874898ae4abf5830c5545748d0d079c9baa1
olawale-o <omoogunolawale@gmail.com>
2025-01-09 05:57:10 +0100

    Fix auth struct

--- src/v1/auth/struct.go
+++ src/v1/auth/struct.go
@@ -1,8 +1,8 @@
 package auth
 
 type LoginAuth struct {
-	USERNAME string `bson:"username" json:"username"`
-	PASSWORD string `bson:"username" json:"password"`
+	USERNAME string `bson:"username" json:"username" validate:"required,min=1"`
+	PASSWORD string `bson:"username" json:"password" validate:"required,min=4"`
 }
 
 type RegisterAuth struct {
