d624f50253151b6414e7bb6ea936bc88aaecfa9c
olawale-o <omoogunolawale@gmail.com>
2025-01-09 05:56:21 +0100

    Create translator wrapper

---
+++ src/v1/translator/translator.go
@@ -0,0 +1,19 @@
+package translator
+
+import (
+	errorTransformer "go-simple-rest/src/v1/error-transformer"
+
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+)
+
+func Translate(validate *validator.Validate, err error) map[string]string {
+	english := en.New()
+	uni := ut.New(english, english)
+	trans, _ := uni.GetTranslator("en")
+	en_translations.RegisterDefaultTranslations(validate, trans)
+	errs := errorTransformer.TranslateErrorToMap(err, trans)
+	return errs
+}
