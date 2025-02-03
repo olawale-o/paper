9191396a1a1443e61ca88bc33d0419832047c954
olawale-o <omoogunolawale@gmail.com>
2025-02-03 14:59:44 +0100

    Modify authors route

--- src/v1/authors/route.go
+++ src/v1/authors/route.go
@@ -3,12 +3,13 @@ package authors
 import "github.com/gin-gonic/gin"
 
 func AuthorRoutes(r *gin.RouterGroup) {
-	r.GET("/authors", Index)
-	r.GET("/authors/:id", Show)
-	r.PUT("/authors/:id", Update)
-	r.DELETE("/authors/:id", Delete)
-	r.GET("/authors/:id/articles", ArticleIndex)
-	r.POST("/authors/:id/articles", ArticleNew)
-	r.PUT("/authors/:id/articles/:articleId", ArticleUpdate)
-	r.DELETE("/authors/:id/articles/:articleId", ArticleDelete)
+	authors := r.Group("/authors")
+	authors.GET("/", Index)
+	authors.GET("/:id", Show)
+	authors.PUT("/:id", Update)
+	authors.DELETE("/:id", Delete)
+	authors.GET("/:id/articles", ArticleIndex)
+	authors.POST("/:id/articles", ArticleNew)
+	authors.PUT("/:id/articles/:articleId", ArticleUpdate)
+	authors.DELETE("/:id/articles/:articleId", ArticleDelete)
 }
