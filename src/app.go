711e74be0818ffc2dbf4c8bc272d62c092905bb8
olawale-o <omoogunolawale@gmail.com>
2025-02-13 04:47:40 +0100

    update: provide required parameters for auth routes

--- src/app.go
+++ src/app.go
@@ -2,18 +2,21 @@ package src
 
 import (
 	"go-simple-rest/src/v1/articles"
-	"go-simple-rest/src/v1/auth"
+	"go-simple-rest/src/v1/auth/route"
+	"go-simple-rest/src/v1/auth/transport"
 	"go-simple-rest/src/v1/authors"
 	"go-simple-rest/src/v1/comment"
 	"go-simple-rest/src/v1/middlewares"
 
+	"github.com/go-kit/log"
+
 	"github.com/gin-gonic/gin"
 )
 
-func Routes(r *gin.Engine) {
+func Routes(r *gin.Engine, svcEndpoint transport.Endpoints, logger log.Logger) {
 	v1 := r.Group("/api/v1")
 	{
-		auth.AuthRoutes(v1)
+		route.AuthRoutes(v1, svcEndpoint, logger)
 		protected := v1.Group("/")
 		protected.Use(middlewares.Auth())
 		articles.ArticleRoutes(protected)
