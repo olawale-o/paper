762f23c55a189c149b7df2dba6f4738a1456c082
olawale-o <omoogunolawale@gmail.com>
2025-02-14 04:52:25 +0100

    feat: setup reverse proxy

--- main.go
+++ main.go
@@ -1,53 +1,19 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"go-simple-rest/db"
-	"go-simple-rest/src"
+	"go-simple-rest/src/routes"
 	"log"
-	"sync"
+	"net/http"
 
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func terminate() {
-	if r := recover(); r != nil {
-		fmt.Println("An error occured ", r)
-		fmt.Println("Application terminated gracefully")
-	} else {
-		fmt.Println("Application executed succcesfully")
-	}
-}
-
-func iterateChangeStream(routineCtx context.Context, waitGroup sync.WaitGroup, stream *mongo.ChangeStream) {
-	defer stream.Close(routineCtx)
-	defer waitGroup.Done()
-	for stream.Next(routineCtx) {
-		var data bson.M
-		if err := stream.Decode(&data); err != nil {
-			panic(err)
-		}
-		fmt.Printf("%v\n", data)
-	}
-}
-
-func init() {
-
-	// defer terminate()
-	db.Connect()
-	// defer func() {
-	// 	if err = client.Disconnect(ctx); err != nil {
-	// 		panic(err)
-	// 	}
-	// }()
-}
-
 func main() {
-	r := gin.Default()
-	src.Routes(r)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	routes.Routes(r)
 	log.Println("Starting server... on port ", 8080)
-	r.Run("127.0.0.1:8080")
+	http.ListenAndServe(":8080", r) // Gateway listens on port 8000
 }
