df7a06a9bd1b4bb7f64fddce468a05a2053f87bb
olawale-o <omoogunolawale@gmail.com>
2025-02-19 15:11:39 +0100

    update: add more struct types

--- src/v1/auth/struct.go
+++ src/v1/auth/struct.go
@@ -5,9 +5,24 @@ import (
 	"go-simple-rest/src/v1/authors"
 )
 
+// LoginAuth
+// @Description: Request struct for login authentication
 type LoginAuth struct {
+	// username
 	USERNAME string `bson:"username" json:"username" validate:"required,min=1"`
+	// password
 	PASSWORD string `bson:"username" json:"password" validate:"required,min=4"`
+}
+
+type User struct {
+	USERNAME string `bson:"username" json:"username"`
+	ROLE     string `bson:"role" json:"role"`
+	ID       string `bson:"id" json:"id"`
+}
+
+type LoginResponse struct {
+	MESSAGE string `json:"message"`
+	USER    User   `json:"user"`
 }
 
 type RegisterAuth struct {
@@ -16,7 +31,6 @@ type RegisterAuth struct {
 	FIRSTNAME string `bson:"firstname" json:"firstname" validate:"required"`
 	LASTNAME  string `bson:"lastname" json:"lastname" validate:"required"`
 }
-
 
 type Repository interface {
 	GetUser(ctx context.Context, collection string, username string) (authors.User, error)
