69e32d80b944b2e7458091a2bfb5a2900135c10e
olawale-o <omoogunolawale@gmail.com>
2025-02-19 15:10:01 +0100

    update: add general api annotation to main package

--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"fmt"
 	"go-simple-rest/db"
+	docs "go-simple-rest/docs"
 	"go-simple-rest/src"
 	"log"
 	"sync"
 
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+
+	swaggerFiles "github.com/swaggo/files" // swagger embed files
 )
 
 func terminate() {
@@ -45,9 +49,40 @@ func init() {
 	// }()
 }
 
+//	@title			Swagger Example API
+//	@version		1.0
+//	@description	This is a sample server celler server.
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@host		localhost:8080
+//	@BasePath	/api/v1
+
+//	@securityDefinitions.basic	BasicAuth
+
+//	@externalDocs.description	OpenAPI
+//	@externalDocs.url			https://swagger.io/resources/open-api/
+
+// @license.name	Apache 2.0
+// @license.url	http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
 	r := gin.Default()
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "A simple REST API for managing users and posts."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "127.0.0.1:8080"
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	src.Routes(r)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	log.Println("Starting server... on port ", 8080)
 	r.Run("127.0.0.1:8080")
+
 }
