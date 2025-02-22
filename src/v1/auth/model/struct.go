a52914c09674db7d4bab3f06da24fc1f9ed2b7b6
olawale-o <omoogunolawale@gmail.com>
2025-02-22 13:29:21 +0100

    update: remove articles field from User struct

--- src/v1/auth/model/struct.go
+++ src/v1/auth/model/struct.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"context"
-	"go-simple-rest/src/v1/articles"
 )
 
 // LoginAuth
@@ -33,16 +32,15 @@ type RegisterAuth struct {
 }
 
 type User struct {
-	ID                string             `bson:"_id,omitempty" json:"id,omitempty"`
-	FIRSTNAME         string             `bson:"firstName" json:"firstName"`
-	LASTNAME          string             `bson:"lastName" json:"lastName"`
-	USERNAME          string             `bson:"username" json:"username"`
-	PASSWORD          string             `bson:"password" json:"password"`
-	ARTICLECOUNT      int                `bson:"articleCount,omitempty" json:"articleCount,omitempty"`
-	ARTICLELIKESCOUNT int                `bson:"articleLikesCount,omitempty" json:"articleLikesCount,omitempty"`
-	CREATEDAT         string             `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
-	ARTICLES          []articles.Article `bson:"articles,omitempty" json:"articles,omitempty"`
-	ROLE              string             `bson:"role,omitempty" json:"role,omitempty"`
+	ID                string `bson:"_id,omitempty" json:"id,omitempty"`
+	FIRSTNAME         string `bson:"firstName" json:"firstName"`
+	LASTNAME          string `bson:"lastName" json:"lastName"`
+	USERNAME          string `bson:"username" json:"username"`
+	PASSWORD          string `bson:"password" json:"password"`
+	ARTICLECOUNT      int    `bson:"articleCount,omitempty" json:"articleCount,omitempty"`
+	ARTICLELIKESCOUNT int    `bson:"articleLikesCount,omitempty" json:"articleLikesCount,omitempty"`
+	CREATEDAT         string `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	ROLE              string `bson:"role,omitempty" json:"role,omitempty"`
 }
 
 type Repository interface {
