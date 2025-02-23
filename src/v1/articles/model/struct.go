8f9f3ab87b5634287434647c673129b3399b6ab5
olawale-o <omoogunolawale@gmail.com>
2025-02-23 16:52:09 +0100

    update: add comment struct

--- src/v1/articles/model/struct.go
+++ src/v1/articles/model/struct.go
@@ -27,6 +27,19 @@ type ArticleCommentReq struct {
 	BODY string `bson:"body" json:"body"`
 }
 
+type Comment struct {
+	ID              interface{}        `bson:"_id,omitempty" json:"id,omitempty"`
+	ARTICLEID       primitive.ObjectID `bson:"articleId" json:"articleId"`
+	BODY            string             `bson:"body" json:"body"`
+	USERID          primitive.ObjectID `bson:"userId,omitempty" json:"userId,omitempty"`
+	LIKES           int                `bson:"likes,omitempty" json:"likes,omitempty"`
+	CREATEDAT       time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	UPDATEDAT       time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
+	DELETEDAT       time.Time          `bson:"deletedAt,omitempty" json:"deletedAt,omitempty"`
+	STATUS          string             `bson:"status,omitempty" json:"status,omitempty"`
+	PARENTCOMMENTID primitive.ObjectID `bson:"parentCommentId,omitempty" json:"parentCommentId,omitempty"`
+}
+
 type Repository interface {
 	Get(ctx context.Context, collection string, filter bson.M) ([]Article, error)
 	FindOne(ctx context.Context, collection string, filter bson.M, v bson.M) (interface{}, error)
