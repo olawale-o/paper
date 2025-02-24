07b4cca8832ebe9a9734e0d223404c37b5cf5d97
olawale-o <omoogunolawale@gmail.com>
2025-02-24 11:41:39 +0100

    update: enable auth middleware

--- src/v1/apps/articles/route/route.go
+++ src/v1/apps/articles/route/route.go
@@ -2,13 +2,14 @@ package route
 
 import (
 	"articles/controller"
+	"articles/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ArticleRoutes(r *gin.Engine) {
 	// articles := r.Group("/articles")
-	// articles.Use(middlewares.Auth())
+	r.Use(middlewares.Auth())
 	r.GET("/", controller.GetArticles)
 	r.POST("/", controller.NewArticle)
 	r.GET("/:id", controller.ShowArticle)
