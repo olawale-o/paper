2dcb4b79b39729ebc5c2dda83b7e66bb9555567e
olawale-o <omoogunolawale@gmail.com>
2025-03-14 04:47:03 +0100

    update: use kafkaclient

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 	"go-simple-rest/db"
-	docs "go-simple-rest/docs"
 	"go-simple-rest/src"
-	"go-simple-rest/src/v1/natsclient"
+	"go-simple-rest/src/swagger"
+	"go-simple-rest/src/v1/kafkaclient"
 	"log"
 	"sync"
 
@@ -17,40 +17,6 @@ import (
 
 	swaggerFiles "github.com/swaggo/files" // swagger embed files
 )
-
-func terminate() {
-	if r := recover(); r != nil {
-		fmt.Println("An error occured ", r)
-		fmt.Println("Application terminated gracefully")
-	} else {
-		fmt.Println("Application executed succcesfully")
-	}
-}
-
-func iterateChangeStream(routineCtx context.Context, waitGroup sync.WaitGroup, stream *mongo.ChangeStream) {
-	defer stream.Close(routineCtx)
-	defer waitGroup.Done()
-	for stream.Next(routineCtx) {
-		var data bson.M
-		if err := stream.Decode(&data); err != nil {
-			panic(err)
-		}
-		fmt.Printf("%v\n", data)
-	}
-}
-
-func init() {
-
-	// defer terminate()
-	db.Connect()
-	// defer func() {
-	// 	if err = client.Disconnect(ctx); err != nil {
-	// 		panic(err)
-	// 	}
-	// }()
-	//
-
-}
 
 //	@title			Swagger Example API
 //	@version		1.0
@@ -75,25 +41,47 @@ func init() {
 // @license.name	Apache 2.0
 // @license.url	http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
+	initializeDatabase()
+	swagger.Initialize()
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-	docs.SwaggerInfo.Title = "Swagger Example API"
-	docs.SwaggerInfo.Description = "A simple REST API for managing users and posts."
-	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "127.0.0.1:8080"
-	docs.SwaggerInfo.BasePath = "/api/v1"
-	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
 	src.Routes(r)
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	cc, err := natsclient.Consumer()
-	if err != nil {
-		log.Println(err)
-	}
-	defer cc.Stop()
-
+	// cc, err := natsclient.Consumer()
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// defer cc.Stop()
+	//
+	go kafkaclient.ConsumerWithAutoCommit("test-group")
 	log.Println("Starting server... on port ", 8080)
 	r.Run("127.0.0.1:8080")
 
+}
+
+func terminate() {
+	if r := recover(); r != nil {
+		fmt.Println("An error occured ", r)
+		fmt.Println("Application terminated gracefully")
+	} else {
+		fmt.Println("Application executed succcesfully")
+	}
+}
+
+func iterateChangeStream(routineCtx context.Context, waitGroup sync.WaitGroup, stream *mongo.ChangeStream) {
+	defer stream.Close(routineCtx)
+	defer waitGroup.Done()
+	for stream.Next(routineCtx) {
+		var data bson.M
+		if err := stream.Decode(&data); err != nil {
+			panic(err)
+		}
+		fmt.Printf("%v\n", data)
+	}
+}
+
+func initializeDatabase() {
+	db.Connect()
 }
