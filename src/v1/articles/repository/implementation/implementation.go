9d34b449fe0f9b08b7a6bb3185ce8d3ed758d84d
olawale-o <omoogunolawale@gmail.com>
2025-03-14 04:48:35 +0100

    add: create repositiory interface and its implementation

--- src/v1/articles/repository/implementation/implementation.go
+++ src/v1/articles/repository/implementation/implementation.go
@@ -1,10 +1,11 @@
-package repo
+package implementation
 
 import (
 	"context"
 	"fmt"
 	"go-simple-rest/db"
 	"go-simple-rest/src/v1/articles/model"
+	"go-simple-rest/src/v1/articles/repository"
 	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -16,18 +17,18 @@ var client, ctx, err = db.Connect()
 
 var collection = client.Database("go").Collection("articles")
 
-type repository struct {
+type RepositoryManager struct {
 	db     *mongo.Database
 	logger log.Logger
 }
 
-func New(db *mongo.Database) (model.Repository, error) {
-	return &repository{
+func New(db *mongo.Database) (repository.Repository, error) {
+	return &RepositoryManager{
 		db: db,
 	}, nil
 }
 
-func (repo *repository) Get(ctx context.Context, collection string, filter bson.M, sort bson.M, opts bson.M) ([]model.Article, error) {
+func (repo *RepositoryManager) Get(ctx context.Context, collection string, filter bson.M, sort bson.M, opts bson.M) ([]model.Article, error) {
 	options := options.Find().SetSort(sort).SetProjection(opts)
 	cursor, err := repo.db.Collection(collection).Find(context.TODO(), filter, options)
 
@@ -42,7 +43,7 @@ func (repo *repository) Get(ctx context.Context, collection string, filter bson.
 	return articles, nil
 }
 
-func (repo *repository) FindOne(ctx context.Context, collection string, filter bson.M, v bson.M, opts bson.M) (interface{}, error) {
+func (repo *RepositoryManager) FindOne(ctx context.Context, collection string, filter bson.M, v bson.M, opts bson.M) (interface{}, error) {
 	options := options.FindOne().SetProjection(opts)
 	if err := repo.db.Collection(collection).FindOne(context.TODO(), filter, options).Decode(&v); err != nil {
 		return nil, err
@@ -50,7 +51,7 @@ func (repo *repository) FindOne(ctx context.Context, collection string, filter b
 	return v, nil
 }
 
-func (repo *repository) InsertOne(ctx context.Context, collection string, doc interface{}) (interface{}, error) {
+func (repo *RepositoryManager) InsertOne(ctx context.Context, collection string, doc interface{}) (interface{}, error) {
 	res, err := repo.db.Collection(collection).InsertOne(context.TODO(), doc)
 	if err != nil {
 		return "", err
@@ -58,7 +59,7 @@ func (repo *repository) InsertOne(ctx context.Context, collection string, doc in
 	return res.InsertedID, nil
 }
 
-func (repo *repository) FindOneAndUpdate(ctx context.Context, collection string, filter bson.M, update bson.M, upsert bool) (interface{}, error) {
+func (repo *RepositoryManager) FindOneAndUpdate(ctx context.Context, collection string, filter bson.M, update bson.M, upsert bool) (interface{}, error) {
 	var data interface{}
 	opts := options.FindOneAndUpdate().SetUpsert(upsert)
 	repo.db.Collection(collection).FindOneAndUpdate(context.TODO(), filter, update, opts).Decode(&data)
@@ -69,7 +70,7 @@ func (repo *repository) FindOneAndUpdate(ctx context.Context, collection string,
 	return data, nil
 }
 
-func (repo *repository) DeleteOne(ctx context.Context, collection string, filter bson.M) error {
+func (repo *RepositoryManager) DeleteOne(ctx context.Context, collection string, filter bson.M) error {
 	res, err := repo.db.Collection(collection).DeleteOne(context.TODO(), filter)
 	if err != nil {
 		return err
@@ -80,7 +81,7 @@ func (repo *repository) DeleteOne(ctx context.Context, collection string, filter
 	return nil
 }
 
-func (repo *repository) UpdateOne(ctx context.Context, collection string, filter bson.M, update bson.M, upsert bool) (interface{}, error) {
+func (repo *RepositoryManager) UpdateOne(ctx context.Context, collection string, filter bson.M, update bson.M, upsert bool) (interface{}, error) {
 	opts := options.Update().SetUpsert(upsert)
 	result, err := repo.db.Collection(collection).UpdateOne(context.TODO(), filter, update, opts)
 
---
+++ src/v1/articles/repository/repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"context"
+	"go-simple-rest/src/v1/articles/model"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type Repository interface {
+	Get(ctx context.Context, collection string, filter bson.M, sort bson.M, opts bson.M) ([]model.Article, error)
+	FindOne(ctx context.Context, collection string, filter bson.M, v bson.M, opts bson.M) (interface{}, error)
+	InsertOne(ctx context.Context, collection string, doc interface{}) (interface{}, error)
+	FindOneAndUpdate(ctx context.Context, collection string, filter bson.M, update bson.M, upsert bool) (interface{}, error)
+	DeleteOne(ctx context.Context, collection string, filter bson.M) error
+	UpdateOne(ctx context.Context, collection string, filter bson.M, update bson.M, upsert bool) (interface{}, error)
+}
