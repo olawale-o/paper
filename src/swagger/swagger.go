c37ad5456a7114eb6ae94596c4d578cfb1b70aaa
olawale-o <omoogunolawale@gmail.com>
2025-03-14 03:14:41 +0100

    docs: create swagger config

---
+++ src/swagger/swagger.go
@@ -0,0 +1,12 @@
+package swagger
+
+import "go-simple-rest/docs"
+
+func Config() {
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "A simple REST API for managing users and posts."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "127.0.0.1:8080"
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+}
