8537cbdd1165b25e6bfd46ff1d3c709e9fc2d242
olawale-o <omoogunolawale@gmail.com>
2025-04-22 15:26:42 +0100

    use: RequestToJSON and validator middlewares

--- src/v1/auth/route/route.go
+++ src/v1/auth/route/route.go
@@ -2,12 +2,17 @@ package route
 
 import (
 	"go-simple-rest/src/v1/auth/controller"
+	"go-simple-rest/src/v1/auth/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func AuthRoutes(r *gin.RouterGroup) {
 	// r.Use(middlewares.Logger())
-	r.POST("/auth/login", controller.Login)
+
+	r.Use(middlewares.RequestToJSON(), middlewares.Validator())
+	{
+		r.POST("/auth/login", controller.Login)
+	}
 	r.POST("/auth/sign-up", controller.Register)
 }
